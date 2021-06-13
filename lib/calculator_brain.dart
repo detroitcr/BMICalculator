5b8d327fdececbf34dc6ec20b8dd649bc889a316
detroitcr <vikas98cr@gmail.com>
2021-06-13 18:35:08 +0530

    first commit

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.weight, this.height});
+
+  final int height;
+  final int weight;
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.9) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretaion() {
+    if (_bmi >= 25) {
+      return 'Khaana kum kar or exercise jyaada';
+    } else if (_bmi > 18.9) {
+      return 'Fit hein tu';
+    } else {
+      return 'Thoda jyaada kha';
+    }
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0XFF1D1E33);
+const kInactiveCardColor = Color(0XFF111328);
+const kBottomContainerColor = Color(0XFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18,
+  color: Color(0XFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMIResultTextStyle = TextStyle(
+  fontSize: 22.0,
+);
